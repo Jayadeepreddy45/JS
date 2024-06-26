94da415156ac8ff315c2a0b7d431801d3256935d
Jayadeepreddy45 <100551416+Jayadeepreddy45@users.noreply.github.com>
2024-06-26 02:51:27 +0000

    datatypes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3  +  3) 
+
+console.log("Jayadeep")
+
+
+let name = "Jayadeep"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof age); // undefined
+console.log(typeof null); // object
